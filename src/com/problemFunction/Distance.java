54f2c215822387ecb0e261ee9f39a267a28ddcb2
Rushikesh <rushikeshdevale2658@gmail.com>
2022-12-30 15:56:40 +0530

    To know Distance

---
+++ src/com/problemFunction/Distance.java
@@ -0,0 +1,14 @@
+package com.problemFunction;
+import java.util.Scanner;
+public class Distance {
+    public static void main(String[] args) {
+            Scanner sc= new Scanner(System.in);
+            System.out.println("Enter the value of X point");
+            int x=sc.nextInt();
+            System.out.println("Enter the value of Y point");
+            int y=sc.nextInt();
+            sc.close();
+            float distance = (float) Math.sqrt((x * x) + (y * y));
+            System.out.println("Euclidean Distance of (" + x + "," + y + ") is " + distance);
+        }
+}
